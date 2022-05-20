6406551ff19876baf28a5e31c5ce290a31936152
FENG Yurong <454548673@qq.com>
2022-05-20 14:52:54 +0800

    update

--- bag/record/c0_depth.sh
+++ bag/record/c0_depth.sh
@@ -1,2 +1,4 @@
-rosbag record -O c0_depth.bag -p /Agent0/camera/infra1/camera_info /Agent0/camera/infra1/image_rect_raw /Agent0/camera/infra2/camera_info /Agent0/camera/depth/image_rect_raw /Agent0/camera/imu
+rosbag record -O c0_depth.bag -p /Agent0/camera/infra1/camera_info /Agent0/camera/infra1/image_rect_raw /Agent0/camera/infra2/camera_info /Agent0/camera/depth/image_rect_raw /Agent0/camera/imu /Agent0/mavros/imu/data_raw
+
+
 
